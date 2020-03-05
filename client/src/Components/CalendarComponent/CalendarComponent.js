6aa729ed131986423d9f272097d26bed7e09cbc1
David Wu <davidjwu92@gmail.com>
2020-03-05 15:13:00 -0800

    CalendarCompoent made, not functioning

---
+++ client/src/Components/CalendarComponent/CalendarComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CalendarComponent = () => {
+
+  return (
+    <>
+        <h5>Calendar Component</h5>
+    </>
+  )
+}
+
+export default CalendarComponent
--- client/src/Pages/MyCalendar/MyCalendar.js
+++ client/src/Pages/MyCalendar/MyCalendar.js
@@ -5,17 +5,22 @@ import React, { useState } from 'react'
 
 // import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 import './myCalendar.css'
 
+import CalendarComponent from '../../Components/CalendarComponent'
 
 const MyCalendar = () => {
   return(
     <>
-      <h1>My Calendar View</h1>
-      <button>Create a Payment Schedule</button>
-      
-      <p>Here we see the calendar (monthly) that color-codes and displays all your upcoming scheduled payments.</p>
+      <div className="container">
+        <h1>My Calendar View</h1>
+        <button>Create a Payment Schedule</button>
+        <p>Here we see the calendar (monthly) that color-codes and displays all your upcoming scheduled payments.</p>
+        
+        <div className = "row">
+          <CalendarComponent/>
+        </div>
+      </div>
     </>
   )
 }
