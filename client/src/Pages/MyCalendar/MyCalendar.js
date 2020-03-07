2780c0d0c42776952eded033c349a2de8fbf5e72
David Wu <davidjwu92@gmail.com>
2020-03-07 12:21:52 -0800

    Experimenting with MyCalendar

--- client/src/Pages/MyCalendar/MyCalendar.js
+++ client/src/Pages/MyCalendar/MyCalendar.js
@@ -9,9 +9,9 @@ import './myCalendar.css';
 import '../../App.css'
 
 //FullCalendar Imports
-import FullCalendar from '@fullcalendar/react'
+import FullCalendar, {eventClickInfo} from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from "@fullcalendar/interaction"
+import interactionPlugin, { Draggable } from "@fullcalendar/interaction"
 //https://fullcalendar.io/docs#toc
 
 
@@ -19,35 +19,96 @@ const MyCalendar = () => {
 
   //Set up Page's Variables
   const [calendarState, setCalendarState] = useState({
+    // EVENT OBJECTS to throw into calendar. "All properties are read-only, use methods to modify."
+    //https://fullcalendar.io/docs/event-object
     payments: [
-      { title: 'EVENT ONE', date: '2020-03-05' },
-      { title: 'EVENT TWO', date: '2020-03-07' }
-    ]
+      //FIRST PAYMENT INSTANCE
+      { id: 0,
+        groupId: 'Car Insurance', //groups payments to be dragged
+        title: 'EVENT ONE', 
+        date: '2020-03-05',
+        allDay: true, 
+        // url: 'www.google.com',  this sends user to google upon clicking event.
+        classNames: ['Insurance'],
+        editable: true,
+        backgroundColor: 'blue',
+        borderColor: 'black',
+        textColor: 'red',
+        extendedProps: {
+          amountDue: '$200',
+          paymentNotes: 'Notes to self: I hate this insurance and I need a new one.',
+          url: 'www.google.com'
+        }
+      },
+      { id: 1,
+        title: 'EVENT TWO',
+        date: '2020-03-07'
+      },
+      {
+        title: `Lil Jon's Piano Lessons`,
+        groupId: 'pianoLessons', // recurrent events in this group move together
+        daysOfWeek: [ '4' ], 
+        allDay: true, 
+        classNames: ['Family'],
+        endRecur: '2020-12-25' //no more piano lessons after Christmas?
+      },
+    ],
+    tagColors: [{insurance: "blue"}, {family: "orange"}, {house: "purple"}, {income: "green"}],
   })
 
   const handleDateClick = (e) =>{
     console.log(e) //Gives me a fat object
   }
   
+  const handleEventClick = (e) => {
+    console.log(e)
+    console.log(e.event.extendedProps)
+    //gives {event, el (html element), jsEvent (click info), view (current view object)}
+    //Ideally triggers a viewing/editing modal.
+  }
+
   return(
     <>
       <div className="container">
-        <h1>My Calendar View</h1>
-        <button>Create a Payment Schedule</button>
-        <p>Here we see the calendar (monthly) that color-codes and displays all your upcoming scheduled payments.</p>
-        
-        <div className = "row">
-        <FullCalendar 
-          defaultView="dayGridMonth"
-          header={{
-            center: 'dayGridMonth,dayGridWeek,dayGridYear',
-          }}
-          plugins={[ dayGridPlugin, interactionPlugin ]}
-          dateClick={handleDateClick}
-          events={calendarState.payments}
-        />
+        {/* PAGE HEADER */}
+        <h3 className = 'center'>Upcoming Payments</h3>
+        <div className = "row"> 
+          <div className = "col s6 m6 l6 center">
+            {/* MODAL with New Payment Form */}
+            <button>Add a new payment</button>
+          </div>
+          <div className = "col s6 m6 l6 center">
+            {/* TAKE ME TO YEAR-VIEW? */}
+            <button>Go to Annual View</button>
+          </div>
         </div>
-      </div>
+        
+        {/* CALENDAR CUSTOMIZATION */}
+        <div className="row">
+          <p>Change the colors of events that have certain tags.</p>
+          {calendarState.tagColors.map(tag => (
+            <>
+              {tag[0]}
+            </>
+          ))}
+        </div>
+        
+        {/* CALENDAR STUFF (contained in div.row) */}
+        <div className = "row">
+          <FullCalendar 
+            defaultView="dayGridMonth"
+            header={{
+              center: 'dayGridMonth,dayGridWeek',
+            }}
+            droppable= {true} //lets me drag and drop events.
+            plugins={[ dayGridPlugin, interactionPlugin ]}
+            dateClick={handleDateClick}
+            eventClick={handleEventClick}
+            events={calendarState.payments} //array of events rendered on calendar.
+          />
+        </div>
+        
+      </div> {/* END CONTAINER */}
     </>
   )
 }
