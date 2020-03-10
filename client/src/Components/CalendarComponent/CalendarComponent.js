83cc3ea730deaed947d8ffb7ee922fea542b1e11
David Wu <davidjwu92@gmail.com>
2020-03-09 17:07:45 -0700

    CalendarComponent for reference

--- client/src/Components/CalendarComponent/CalendarComponent.js
+++ client/src/Components/CalendarComponent/CalendarComponent.js
@@ -10,7 +10,33 @@ import interactionPlugin from "@fullcalendar/interaction"
 const CalendarComponent = () => {
   
   const [componentState, setComponentState] = useState({
-    payments: []
+    payments: [ //FIRST PAYMENT INSTANCE
+      // { id: 0,
+      //   groupId: 'Car Insurance', //groups events to be dragged
+      //   title: 'EVENT ONE', 
+      //   date: '2020-03-05',
+      //   allDay: true, 
+      //   classNames: ['Insurance'],
+      //   editable: true,
+      //   backgroundColor: 'blue',
+      //   borderColor: 'black',
+      //   textColor: 'red',
+      //   extendedProps: {
+      //     amount: '$200',
+      //     category: 'Family',
+      //     paymentNotes: 'I hate this insurance and I need a new one.',
+      //     url: 'www.google.com'
+      //   }
+      // },
+      // {
+      //   title: `Lil Jon's Piano Lessons`,
+      //   groupId: 'pianoLessons', // recurrent events in this group move together
+      //   daysOfWeek: [ '4' ],  //WEEKLY stuff ONLY.
+      //   allDay: true, 
+      //   classNames: ['Family'],
+      //   endRecur: '2020-12-25' //no more piano lessons after Christmas.
+      // },
+    ]
   })
 
   const handleDateClick = (e) =>{
