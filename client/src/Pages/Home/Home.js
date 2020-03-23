c9ea910c4f577eeea184f9c4eb2c45d7097a0729
David Wu <davidjwu92@gmail.com>
2020-03-23 10:37:01 -0700

    Home Page work

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -12,9 +12,21 @@ const Home = () => {
 
   return (
     <>
-      <div className="container center-align">
-        <h5 className="white-text">HOME PAGE</h5>
-        <button onClick={toLoginPage}>Get Started</button>
+      <div className="white-text container center-align">
+        <h2 className="white-text">Welcome to PayTrack</h2>
+        <h5 className="white-text">The ultimate calendar-based budgeting app.</h5>
+        <div className="row left-align">
+          <div className="col s12 m4 l4">
+            <h6>Track recurring or one-time payments on a customizable, color-coded budget calendar.</h6>
+          </div>
+          <div className="col s12 m4 l4">
+            <h6>See automatically generated monthly, yearly, and/or category-based statements summarizing your expenditures and income.</h6>
+          </div>
+          <div className="col s12 m4 l4">
+            <h6>Have access to graphs and charts of your financial activity.</h6>
+          </div>
+        </div>
+        <button className="btn btn-large purple"onClick={toLoginPage}>Get Started</button>
       </div>
     </>
   )
